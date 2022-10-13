6a0e9157c2e2b0f5cfc36b8223f82e151d3691bf
Nonnie2022 <lindaikedigwe@gmail.com>
2022-10-13 10:57:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * Main ==> the entry point for the program, otherwise called function.
- * Return ==> Marks the end of the program, and returns 0 if there are no error
- * otherwise it returns non-zero
+/**
+ * Main - this program will print programming is like building a multilingual puzzle
+ * Return - this returns zero if there is no error
  */
 int main (void)
 {
-	puts ("/"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("/"Programming is like building a multilingual puzzle\n");
+	return (0);
 }

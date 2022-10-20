3fd6d4b50ce6274695ef9a0300a344adb25fc719
Nonnie2022 <lindaikedigwe@gmail.com>
2022-10-20 06:24:17 -0700

    task o

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* positive_or_negative - prints if integer is positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
